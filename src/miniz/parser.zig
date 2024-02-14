63ae9d57e6a4ef28d2ad76aefbc137f937c7ff83
Ryoga-exe <git@ryoga.dev>
2024-02-15 01:10:42 +0900

    feat: add atom parser

--- src/miniz/parser.zig
+++ src/miniz/parser.zig
@@ -1,4 +1,33 @@
 const std = @import("std");
 const ast = @import("./ast.zig");
+const TokenType = @import("./token.zig").TokenType;
+const Lexer = @import("./lexer.zig").Lexer;
 
-pub fn parse_atom() ast.SExpr {}
+pub fn parse_atom(lexer: *Lexer) ast.SExpr {
+    const tok = lexer.nextToken();
+    switch (tok.type) {
+        TokenType.integer => {
+            return ast.SExpr{ .atom = tok.literal };
+        },
+        else => {
+            // Todo
+            return ast.SExpr{ .atom = tok.literal };
+        },
+    }
+}
+
+pub fn parse_expr(lexer: *Lexer) ast.SExpr {
+    // Todo
+    return parse_atom(lexer);
+}
+
+test "Parse atom" {
+    const input = "7";
+    var lexer = Lexer.init(input);
+    const e = parse_expr(&lexer);
+
+    const str = try e.toString(std.testing.allocator);
+    defer std.testing.allocator.free(str);
+
+    try std.testing.expectEqualSlices(u8, "7", str);
+}
