7c6773028ea80614251233dd03f351a90614c143
Ryoga-exe <git@ryoga.dev>
2024-02-15 01:21:41 +0900

    refactor

--- src/miniz/parser.zig
+++ src/miniz/parser.zig
@@ -1,33 +1,32 @@
 const std = @import("std");
-const ast = @import("./ast.zig");
+const SExpr = @import("./ast.zig").SExpr;
 const TokenType = @import("./token.zig").TokenType;
 const Lexer = @import("./lexer.zig").Lexer;
 
-pub fn parse_atom(lexer: *Lexer) ast.SExpr {
+pub fn parse_atom(lexer: *Lexer) SExpr {
     const tok = lexer.nextToken();
     switch (tok.type) {
         TokenType.integer => {
-            return ast.SExpr{ .atom = tok.literal };
+            return SExpr{ .atom = tok.literal };
         },
         else => {
             // Todo
-            return ast.SExpr{ .atom = tok.literal };
+            return SExpr{ .atom = tok.literal };
         },
     }
 }
 
-pub fn parse_expr(lexer: *Lexer) ast.SExpr {
+pub fn parse_expr(lexer: *Lexer) SExpr {
     // Todo
     return parse_atom(lexer);
 }
 
 test "Parse atom" {
+    const alloc = std.testing.allocator;
     const input = "7";
     var lexer = Lexer.init(input);
     const e = parse_expr(&lexer);
-
-    const str = try e.toString(std.testing.allocator);
-    defer std.testing.allocator.free(str);
-
+    const str = try e.toString(alloc);
+    defer alloc.free(str);
     try std.testing.expectEqualSlices(u8, "7", str);
 }
