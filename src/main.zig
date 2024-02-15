aac0f664494018983f7ac52fab009c09619f67c0
Ryoga-exe <git@ryoga.dev>
2024-02-16 05:08:08 +0900

    feat: add prompt

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,19 @@
 const std = @import("std");
+const repl = @import("miniz/repl.zig");
 
 pub fn main() !void {
     const alc = std.heap.page_allocator;
     const args = try std.process.argsAlloc(alc);
     defer std.process.argsFree(alc, args);
 
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const stdout = std.io.getStdOut().writer();
 
     if (args.len < 2) {
-        try stdout.print("repl mode...\n", .{});
-        try bw.flush();
+        try repl.start(alc);
         std.os.exit(0);
     }
 
     try stdout.print("input file: {s}\n", .{args[1]});
-    try bw.flush();
 
     // if .json
     // if .mnz or .miniz
--- src/miniz/repl.zig
+++ src/miniz/repl.zig
@@ -1,1 +1,46 @@
-const PROMPT = "> ";
+const std = @import("std");
+const Prompt = "> ";
+const BufferSize = 8192;
+
+pub fn start(allocator: std.mem.Allocator) !void {
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+
+    const welcome_message =
+        \\ 
+        \\ ███╗░░░███╗██╗███╗░░██╗██╗███████╗
+        \\ ████╗░████║██║████╗░██║██║╚════██║
+        \\ ██╔████╔██║██║██╔██╗██║██║░░███╔═╝
+        \\ ██║╚██╔╝██║██║██║╚████║██║██╔══╝░░
+        \\ ██║░╚═╝░██║██║██║░╚███║██║███████╗
+        \\ ╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚═╝╚══════╝
+        \\ 
+        \\ Minimal scripting language in Zig.
+        \\ 
+    ;
+
+    try stdout.print("{s}\n", .{welcome_message});
+    // var env = ...
+    while (true) {
+        try stdout.print("{s}", .{Prompt});
+        const line = readLine(allocator, stdin) catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => return err,
+        };
+        if (line) |ln| {
+            if (ln.len == 0) {
+                continue;
+            }
+            try stdout.print("{s}\n", .{ln});
+            // const result = eval(...)
+            // try stdout.print("{s}\n", result.toString()...)
+        }
+    }
+}
+
+fn readLine(allocator: std.mem.Allocator, reader: anytype) !?[]const u8 {
+    var buffer = try allocator.alloc(u8, BufferSize);
+    var fbs = std.io.fixedBufferStream(buffer);
+    try reader.streamUntilDelimiter(fbs.writer(), '\n', BufferSize);
+    return fbs.getWritten();
+}
