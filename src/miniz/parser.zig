c42b419dd3c358abc1c5d54f52d3ecab9c42c5e4
Ryoga-exe <git@ryoga.dev>
2024-02-16 08:55:43 +0900

    feat: add some parser

--- src/miniz/parser.zig
+++ src/miniz/parser.zig
@@ -1,32 +1,114 @@
 const std = @import("std");
-const SExpr = @import("./ast.zig").SExpr;
-const TokenType = @import("./token.zig").TokenType;
-const Lexer = @import("./lexer.zig").Lexer;
+const ast = @import("ast.zig");
+const Token = @import("token.zig").Token;
+const Lexer = @import("lexer.zig").Lexer;
+const Allocator = std.mem.Allocator;
 
-pub fn parse_atom(lexer: *Lexer) SExpr {
-    const tok = lexer.nextToken();
-    switch (tok.type) {
-        TokenType.integer => {
-            return SExpr{ .atom = tok.literal };
-        },
-        else => {
-            // Todo
-            return SExpr{ .atom = tok.literal };
-        },
+const Parser = struct {
+    const Self = @This();
+    allocator: Allocator,
+    lexer: *Lexer,
+    currentToken: Token,
+    peekToken: Token,
+
+    pub fn init(allocator: Allocator, lexer: *Lexer) Self {
+        var p = Self{
+            .allocator = allocator,
+            .lexer = lexer,
+            .currentToken = undefined,
+            .peekToken = undefined,
+        };
+        p.nextToken();
+        p.nextToken();
+        return p;
     }
-}
+    pub fn deinit(self: *Self) void {
+        _ = self;
+    }
+    pub fn parseProgram(self: *Self) !*ast.SExpr {
+        return self.parseExpr();
+    }
+    fn parseExpr(self: *Self) !*ast.SExpr {
+        switch (self.currentToken.type) {
+            .minus => {
+                self.nextToken();
+                const following = try self.parseExpr();
+                return try ast.SExpr.createList(self.allocator, &[_]*ast.SExpr{ try ast.SExpr.createAtom(self.allocator, "-"), following });
+            },
+            .lparen => {
+                self.nextToken();
+                const following = try self.parseExpr();
+                if (self.currentToken.type != .rparen) {
+                    unreachable;
+                }
+                self.nextToken();
 
-pub fn parse_expr(lexer: *Lexer) SExpr {
-    // Todo
-    return parse_atom(lexer);
-}
+                return try ast.SExpr.createList(self.allocator, &[_]*ast.SExpr{ try ast.SExpr.createAtom(self.allocator, "paren"), following });
+            },
+            else => {
+                return self.parseAtom();
+            },
+        }
+    }
+    fn parseAtom(self: *Self) !*ast.SExpr {
+        switch (self.currentToken.type) {
+            .integer => {
+                const result = try ast.SExpr.createAtom(self.allocator, self.currentToken.literal);
+                self.nextToken();
+                return result;
+            },
+            else => {
+                unreachable;
+            },
+        }
+    }
+    fn parseExprBp(self: *Self) ast.SExpr {
+        _ = self;
+    }
+    fn nextToken(self: *Self) void {
+        self.currentToken = self.peekToken;
+        self.peekToken = self.lexer.nextToken();
+    }
+};
 
-test "Parse atom" {
+test "parse: 1234" {
     const alloc = std.testing.allocator;
-    const input = "7";
+    const input = "1234";
     var lexer = Lexer.init(input);
-    const e = parse_expr(&lexer);
+    var parser = Parser.init(alloc, &lexer);
+    defer parser.deinit();
+    const e = try parser.parseProgram();
+    defer e.deinit(alloc);
     const str = try e.toString(alloc);
     defer alloc.free(str);
-    try std.testing.expectEqualSlices(u8, "7", str);
+
+    try std.testing.expectEqualSlices(u8, "1234", str);
+}
+
+test "parse: -1234" {
+    const alloc = std.testing.allocator;
+    const input = "-1234";
+    var lexer = Lexer.init(input);
+    var parser = Parser.init(alloc, &lexer);
+    defer parser.deinit();
+    const e = try parser.parseProgram();
+    defer e.deinit(alloc);
+    const str = try e.toString(alloc);
+    defer alloc.free(str);
+
+    try std.testing.expectEqualSlices(u8, "(- 1234)", str);
+}
+
+test "parse (-1234)" {
+    const alloc = std.testing.allocator;
+    const input = "(-1234)";
+    var lexer = Lexer.init(input);
+    var parser = Parser.init(alloc, &lexer);
+    defer parser.deinit();
+    const e = try parser.parseProgram();
+    defer e.deinit(alloc);
+    const str = try e.toString(alloc);
+    defer alloc.free(str);
+
+    try std.testing.expectEqualSlices(u8, "(paren (- 1234))", str);
 }
